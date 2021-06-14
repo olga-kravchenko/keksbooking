36b6fc511af88fdde3a40a56f16a526a9b522a34
Olha Kravchenko <olga19972010@yandex.by>
2021-06-14 16:10:28 +0300

    3.1 Created DOM elements that match the labels on the map, and filled them with data from the array.

--- js/main.js
+++ js/main.js
@@ -6,18 +6,23 @@ const MAX_QUANTITY_OF_ROOMS = 5;
 const MIN_QUANTITY_OF_VISITORS = 1;
 const MAX_QUANTITY_OF_VISITORS = 15;
 const MIN_ARRAY_INDEX = 0;
-const LOCATION_X = 600;
-const LOCATION_Y = 350;
 const MONEY_CONVERTER = 100;
 const MIN_X_COORDINATE = 100;
 const MAX_X_COORDINATE = 1100;
 const MIN_Y_COORDINATE = 130;
 const MAX_Y_COORDINATE = 630;
-
+const SHIFT_PIN_X = 40;
+const SHIFT_PIN_Y = 51;
 const TYPES_OF_APARTMENTS = [`palace`, `flat`, `house`, `bungalow`];
 const TIMES = [`12:00`, `13:00`, `14:00`];
 const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 const PHOTOS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+
+const map = document.querySelector(`.map`);
+const pinTemplate = document.querySelector(`#pin`).content;
+const pins = map.querySelector(`.map__pins`);
+
+map.classList.remove(`map--faded`);
 
 const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
@@ -25,7 +30,6 @@ const getRandomNumber = (min, max) => {
 
 const getAdvertisements = () => {
   let advertisements = [];
-
   for (let i = 0; i < QUANTITY_OF_ADVERTISEMENTS; i++) {
     let number = i + 1;
 
@@ -35,7 +39,6 @@ const getAdvertisements = () => {
       },
       offer: {
         title: `заголовок предложения 0${number}`,
-        address: `${LOCATION_X}, ${LOCATION_Y}`,
         price: number * MONEY_CONVERTER,
         type: TYPES_OF_APARTMENTS[getRandomNumber(0, TYPES_OF_APARTMENTS.length)],
         rooms: getRandomNumber(MIN_QUANTITY_OF_ROOMS, MAX_QUANTITY_OF_ROOMS),
@@ -51,9 +54,31 @@ const getAdvertisements = () => {
         y: getRandomNumber(MIN_Y_COORDINATE, MAX_Y_COORDINATE),
       }
     };
+    advertisement.address = `${advertisement.location.x}, ${advertisement.location.y}`;
     advertisements.push(advertisement);
   }
   return advertisements;
 };
 
-console.log(getAdvertisements());
+const createDomElementPin = (advertisement) => {
+  const newPin = pinTemplate.cloneNode(true);
+  const pinButton = newPin.querySelector(`.map__pin`);
+  const pinImg = pinButton.querySelector(`img`);
+  pinButton.style.left = `${advertisement.location.x + SHIFT_PIN_X}px`;
+  pinButton.style.top = `${advertisement.location.y + SHIFT_PIN_Y}px`;
+  pinImg.src = advertisement.author.avatar;
+  pinImg.alt = `Заголовок объявления`;
+  return newPin;
+};
+
+const fillDomElementsByPin = (arr) => {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < arr.length; i++) {
+    const newPhoto = createDomElementPin(arr[i]);
+    fragment.appendChild(newPhoto);
+  }
+  pins.appendChild(fragment);
+};
+
+const pinsArray = getAdvertisements();
+fillDomElementsByPin(pinsArray);
