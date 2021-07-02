4dc9398f4e730151cfd782ee1df9c40ee190a3e3
Olha Kravchenko <olga19972010@yandex.by>
2021-07-02 16:25:32 +0300

    JQuery-1.1 Сonverted the code of the pin  module to jQuery.

--- js/modules/pin.js
+++ js/modules/pin.js
@@ -3,15 +3,19 @@
 const SHIFT_PIN_X = 40;
 const SHIFT_PIN_Y = 51;
 
-const pinTemplate = document.querySelector(`#pin`).content;
+const pinTemplate = $(`#pin`)[0].content;
 
 const create = (advertisement) => {
-  const newPin = pinTemplate.cloneNode(true);
-  newPin.querySelector(`.map__pin`).dataset.id = advertisement.id;
-  newPin.querySelector(`.map__pin`).style.left = `${advertisement.location.x + SHIFT_PIN_X}px`;
-  newPin.querySelector(`.map__pin`).style.top = `${advertisement.location.y + SHIFT_PIN_Y}px`;
-  newPin.querySelector(`img`).src = advertisement.author.avatar;
-  newPin.querySelector(`img`).alt = `Заголовок объявления`;
+  const newPin = $(pinTemplate.cloneNode(true));
+  const mapPin = newPin.find(`.map__pin`);
+  const mapPinImg = mapPin.find(`img`);
+  mapPin.data(`id`, advertisement.id);
+  mapPin.css({
+    'left': `${advertisement.location.x + SHIFT_PIN_X}px`,
+    'top': `${advertisement.location.y + SHIFT_PIN_Y}px`
+  });
+  mapPinImg.attr(`src`, advertisement.author.avatar);
+  mapPinImg.attr(`alt`, `Заголовок объявления`);
   return newPin;
 };
 
