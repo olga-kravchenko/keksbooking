3200fc1ea25de791458997dd16a08de495716acb
Olha Kravchenko <olga19972010@yandex.by>
2021-07-02 16:04:52 +0300

    JQuery-1.1 Сonverted the code of the preview  module to jQuery.

--- js/modules/preview.js
+++ js/modules/preview.js
@@ -2,48 +2,51 @@
 
 const FILE_TYPES = [`gif`, `jpg`, `jpeg`, `png`];
 
-const fileChooserAvatar = document.querySelector(`#avatar`);
-const previewAvatar = document.querySelector(`.ad-form-header__upload img`);
-const fileChooserImage = document.querySelector(`#images`);
-const previewPicture = document.querySelector(`.ad-form__photo`);
+const $mapImageChooser = $(`#avatar`);
+const $mapImage = $(`.ad-form-header__upload img`);
+const $apartmentImageChooser = $(`#images`);
+const $apartmentImage = $(`.ad-form__photo`);
 
 const reader = new FileReader();
 
-const imageChange = (fileChooser, img) => {
-  const file = fileChooser.files[0];
+const imageChange = ($fileChooser, img) => {
+  const file = $fileChooser[0].files[0];
   const fileName = file.name.toLowerCase();
   const matchingTheFileType = FILE_TYPES.some((e) => fileName.endsWith(e));
   if (matchingTheFileType) {
-    reader.addEventListener(`load`, () => {
-      img.src = reader.result;
+    $(reader).on(`load`, () => {
+      img.attr(`src`, reader.result);
+      img.attr(`alt`, `Загруженное изображение`);
     });
     reader.readAsDataURL(file);
   }
 };
 
 const onFileChooserAvatarChange = () => {
-  imageChange(fileChooserAvatar, previewAvatar);
+  imageChange($mapImageChooser, $mapImage);
 };
 
 const onFileChooserImageChange = () => {
-  const picture = document.createElement(`img`);
-  picture.style.width = `40px`;
-  picture.style.height = `40px`;
-  previewPicture.appendChild(picture);
-  imageChange(fileChooserImage, picture);
+  const picture = $(`<img src="" alt="">`);
+  picture.css({
+    'width': `40px`,
+    'height': `40px`
+  });
+  $apartmentImage.append(picture);
+  imageChange($apartmentImageChooser, picture);
 };
 
 const reset = () => {
-  previewAvatar.src = `img/muffin-grey.svg`;
-  const image = document.querySelector(`.ad-form__photo img`);
-  if (image) {
-    image.remove();
+  $mapImage.attr(`src`, `img/muffin-grey.svg`);
+  const $image = $(`.ad-form__photo img`);
+  if ($image.length) {
+    $image.remove();
   }
 };
 
 const addListeners = () => {
-  fileChooserAvatar.addEventListener(`change`, onFileChooserAvatarChange);
-  fileChooserImage.addEventListener(`change`, onFileChooserImageChange);
+  $mapImageChooser.on(`change`, onFileChooserAvatarChange);
+  $apartmentImageChooser.on(`change`, onFileChooserImageChange);
 };
 
 window.preview = {
