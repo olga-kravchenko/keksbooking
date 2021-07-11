a3d313804efaa76872fbca5dd8bad0ac0550fd44
Olha Kravchenko <olga19972010@yandex.by>
2021-07-11 11:06:42 +0300

    Third attempt:made the order in the card module and simplified the code.

--- js/modules/card.js
+++ js/modules/card.js
@@ -11,35 +11,27 @@ const $map = $(`.map`);
 const $filterContainer = $(`.map__filters-container`);
 const template = $(`#card`)[0].content;
 
-const addFeatures = ($newCard, pinInfo) => {
-  const $featureSection = $newCard.find(`.popup__features`);
-  if (pinInfo.offer.features.length) {
-    $featureSection.empty();
-    const features = pinInfo.offer.features;
-    const $fragment = $(document.createDocumentFragment());
-    features.forEach((feature) => {
-      const $feature = $(`<li class="popup__feature popup__feature--${feature}"></li>`);
-      $fragment.append($feature);
-    });
-    $featureSection.append($fragment);
-  } else {
+const addFeatures = ($featureSection, features) => {
+  if (!features.length) {
     $featureSection.remove();
+  } else {
+    $featureSection.empty();
+    const $fragment = $(document.createDocumentFragment());
+    features.forEach((feature) => $fragment.append($(`<li class="popup__feature popup__feature--${feature}"></li>`)));
+    $featureSection.append($fragment);
   }
 };
 
-const addPhotos = ($newCard, pinInfo) => {
-  const $photosSection = $newCard.find(`.popup__photos`);
-  if (pinInfo.offer.photos.length) {
+const addPhotos = ($photosSection, photos, title) => {
+  if (!photos.length) {
+    $photosSection.remove();
+  } else {
     $photosSection.empty();
-    const photos = pinInfo.offer.photos;
     const $fragment = $(document.createDocumentFragment());
     photos.forEach((photoSrc) => {
-      const $photo = $(`<img class="popup__photo" src="${photoSrc}" alt="${pinInfo.offer.title}" width="45" height="45">`);
-      $fragment.append($photo);
+      $fragment.append($(`<img class="popup__photo" src="${photoSrc}" alt="${title}" width="45" height="45">`));
     });
     $photosSection.append($fragment);
-  } else {
-    $photosSection.remove();
   }
 };
 
@@ -52,38 +44,34 @@ const createAndInit = (pinInfo) => {
   $newCard.find(`.popup__type`).text(ApartmentNames[pinInfo.offer.type]);
   $newCard.find(`.popup__text--capacity`).text(`${pinInfo.offer.rooms} комнаты для ${pinInfo.offer.guests} гостей`);
   $newCard.find(`.popup__text--time`).text(`Заезд после ${pinInfo.offer.checkin}, выезд до ${pinInfo.offer.checkout}`);
-  addFeatures($newCard, pinInfo);
+  addFeatures($newCard.find(`.popup__features`), pinInfo.offer.features);
   $newCard.find(`.popup__description`).text(pinInfo.offer.description);
-  addPhotos($newCard, pinInfo);
+  addPhotos($newCard.find(`.popup__photos`), pinInfo.offer.photos, pinInfo.offer.title);
   return $newCard;
+};
+
+const render = (activePin) => {
+  const newCard = createAndInit(activePin);
+  $map.append(newCard, $filterContainer);
 };
 
 const remove = () => {
   const $card = $(`.map__card.popup`);
   if ($card.length) {
     $card.remove();
+    window.map.removeActiveClass();
+    $(document).off(`keydown`, onEscKeydown);
   }
-};
-
-const render = (activePin) => {
-  const $fragment = $(document.createDocumentFragment());
-  const newCard = createAndInit(activePin);
-  $fragment.append(newCard);
-  $map.append($fragment, $filterContainer);
-};
-
-const onCloseButtonClick = () => {
-  remove();
-  window.map.removeActivePin();
 };
 
 const onEscKeydown = (evt) => {
-  const isEscape = evt.key === `Escape`;
+  const isEscape = evt.key === window.util.BUTTON_ESCAPE;
   if (isEscape) {
     remove();
-    window.map.removeActivePin();
   }
 };
+
+const onCloseButtonClick = () => remove();
 
 const on = () => {
   $(`.popup__close`).on(`click`, onCloseButtonClick);
