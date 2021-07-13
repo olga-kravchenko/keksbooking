4c762b927f58f867d7af159f8dccb85ddefe967d
Olha Kravchenko <olga19972010@yandex.by>
2021-07-13 08:01:32 +0300

    Third attempt:made the order in the util module and simplified the code.

--- js/modules/util.js
+++ js/modules/util.js
@@ -1,15 +1,21 @@
 'use strict';
 
-const ERROR_MODAL_DISPLAY_TIME = 3000;
+const BUTTON_ENTER = `Enter`;
+const BUTTON_ESCAPE = `Escape`;
+const LEFT_BUTTON = 0;
+const ERROR_MODAL_DISPLAY_TIME = 300000;
 
 const showErrorMessage = (errorMessage) => {
   const $errorModal = $(`<div></div>`);
   $errorModal.addClass(`modal-error`);
   $errorModal.text(errorMessage);
-  $(`body`).before($errorModal);
+  $(`body`).prepend($errorModal);
   setTimeout(() => $errorModal.remove(), ERROR_MODAL_DISPLAY_TIME);
 };
 
 window.util = {
+  BUTTON_ENTER,
+  BUTTON_ESCAPE,
+  LEFT_BUTTON,
   showErrorMessage,
 };
